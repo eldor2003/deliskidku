0742a6c1f7ce4dbe152cbc1a1f0fe29e482b7b0a
eldor2003 <qulsoatoveldor@gmail.com>
2023-11-21 15:58:38 +0500

    last updated

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let heroSwiper = new Swiper(".hero_swiper", {
+    slidesPerView: 3.9,
+    spaceBetween: 4,
+    navigation: {
+        nextEl: ".hero_swiper-btnN",
+        prevEl: ".hero_swiper-btnP",
+    },
+  });
