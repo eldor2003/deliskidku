685743635d0f9ebd0ec4a1b8501d4cac061ba1c1
eldor2003 <qulsoatoveldor@gmail.com>
2023-11-25 22:39:10 +0500

    new updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,36 @@
+// header top links
+
+let headerNavLinks = $(".header_nav-link");
+headerNavLinks.each(function (btn) { 
+    $(this).on("click", function (e) {
+        e.preventDefault();
+        $(this).addClass("active");
+        headerNavLinks.not($(this)).removeClass("active");
+    });
+});
+
+// hero swiper
 let heroSwiper = new Swiper(".hero_swiper", {
-    slidesPerView: 3.9,
-    spaceBetween: 4,
     navigation: {
         nextEl: ".hero_swiper-btnN",
         prevEl: ".hero_swiper-btnP",
     },
-  });
+    breakpoints: {
+        320: {
+            slidesPerView: 0.37,
+            spaceBetween: 4,
+        },
+        375: {
+            slidesPerView: 0.57,
+            spaceBetween: 4,
+        },
+        425: {
+            slidesPerView: 0.8,
+            spaceBetween: 4,
+        },
+        1199: {
+            slidesPerView: 3.9,
+            spaceBetween: 4,
+        },
+    },
+});
